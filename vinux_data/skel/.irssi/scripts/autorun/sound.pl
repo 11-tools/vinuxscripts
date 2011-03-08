d133af03bc1a2c515e6eccb3b5b6ac1f12ee27a4
vinux <vinux@vinux.(none)>
2011-03-08 12:41:32 +0000

    Removed simlink from .irssi

--- vinux_data/skel/.irssi/scripts/autorun/sound.pl
+++ vinux_data/skel/.irssi/scripts/autorun/sound.pl
@@ -1,1 +1,51 @@
-../sound.pl
+use strict;
+use vars qw($VERSION %IRSSI);
+
+use Irssi;
+#set your sound playing method here. suggestionns are paplay or play -q if you have sox installed.
+my $soundCommand = "paplay";
+$VERSION = '0.0.3';
+%IRSSI = (
+        authors     => 'Chrelad',
+        contact     => 'blah@blah.blah',
+        name        => 'alert',
+        description => 'Play sounds for different events in IRSSI.',
+        url         => 'http://google.com',
+        license     => 'GNU General Public License',
+        changed     => '$Date: 2007-02-07 12:00:00 +0100 (Thu, 7 Feb 2008) $'
+);
+
+#--------------------------------------------------------------------
+# Created by Chrelad
+# Feb 7, 2008
+#modified by Storm Dragon
+#Added private message sound and the ability to select sound method
+#Sounds now play as a background process.
+#--------------------------------------------------------------------
+
+#--------------------------------------------------------------------
+# The sound playing function for public message
+#--------------------------------------------------------------------
+
+sub pub_msg {
+        my ($server,$msg,$nick,$address,$target) = @_;
+        system("$soundCommand ~/.irssi/scripts/publicmsg.ogg &");
+}
+
+#--------------------------------------------------------------------
+# The sound playing function for private message
+#--------------------------------------------------------------------
+
+sub pri_msg {
+        my ($server,$msg,$nick,$address,$target) = @_;
+        system("$soundCommand ~/.irssi/scripts/privatemsg.ogg &");
+}
+
+#--------------------------------------------------------------------
+# Irssi::signal_add_last / Irssi::command_bind
+#--------------------------------------------------------------------
+
+Irssi::signal_add_last("message public", "pub_msg");
+Irssi::signal_add_last("message private", "pri_msg");
+#- end
+
